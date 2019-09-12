a5bbfbbf7cec47e37c8fccb38d5c9ef8dd9e7d32
summersdani <42975800+summersdani@users.noreply.github.com>
2019-09-12 16:27:08 -0400

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpCalculator
+{
+    public class Calculate
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Oper { get; set; }
+        public double Result { get; set; }
+
+        public double Evaluation()
+        {
+            switch (Oper)
+            {
+                case "+":
+                    Result = FirstNumber + SecondNumber;
+                    break;
+                case "-":
+                    Result = FirstNumber - SecondNumber;
+                    break;
+                case "*":
+                    Result = FirstNumber * SecondNumber;
+                    break;
+                case "/":
+                    Result = FirstNumber / SecondNumber;
+                    break;
+                default:
+                    break;
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace csharpCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculate = new Calculate();
+            Console.WriteLine("Simple Calculator");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Type your first number: ");
+                    calculate.FirstNumber = double.Parse(Console.ReadLine());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+
+                try
+                {
+                    Console.WriteLine("\tTo add enter: \t\t'+'");
+                    Console.WriteLine("\tTo subtract enter: \t'-'");
+                    Console.WriteLine("\tTo multiply enter: \t'*'");
+                    Console.WriteLine("\tTo divide enter: \t'/'");
+                    Console.Write("Enter operator: ");
+                    calculate.Oper = Console.ReadLine();
+
+                    if (calculate.Oper == "+" || calculate.Oper == "-" || calculate.Oper == "*" || calculate.Oper == "/")
+                    {
+                        Console.WriteLine(calculate.Oper);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid operator");
+                        break;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                try
+                {
+                    Console.Write("Type your second number: ");
+                    calculate.SecondNumber = double.Parse(Console.ReadLine());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+
+                var evaluate = calculate.Evaluation();
+
+                Console.WriteLine($"Your Answer: {evaluate}");
+            }
+
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine();
+        }
+    }
+}
